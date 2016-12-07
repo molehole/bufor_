2dd0b3788505167e18fa5248946fe72d15d0611f
Shared IT account <itadmin@jan-svr-intra01.jan.polipol.intra>
2016-12-07 16:42:29 +0100

    Dodane modele

---
+++ bufor/models.py
@@ -0,0 +1,51 @@
+#Modele
+from django.db import models
+from django.utils import timezone
+from datetime import datetime
+
+class Tura(models.Model):
+    nr = models.IntegerField()
+    data = models.DateField()
+
+    def __str__(self):
+        return_string = str(self.nr) + "(" + str(self.data) + ")"
+        return str(return_string)
+
+class TA(models.Model):
+    tura = models.ForeignKey(Tura)
+    nr = models.IntegerField()
+    elementy = models.TextField()
+    zakonczone = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.nr, sefl.zakonczone)
+
+class Etykieta(models.Model):
+    ta = models.ForeignKey(TA)
+    nr = models.IntegerField()
+    pozycja = models.IntegerField(null=True)
+    element = models.TextField(null=True)
+
+    def __str__(self):
+        return str(self.nr)
+
+#Tabela Statusów
+class Status(models.Model):
+    ta = models.ForeignKey(TA)
+    szwalnia = models.BooleanField(default=False)
+    stolarnia = models.BooleanField(default=False)
+    tapicernia = models.BooleanField(default=False)
+    szwalnia_ilosc = models.IntegerField(default=1)
+    stolarnia_ilosc = models.IntegerField(default=1)
+    tapicernia_ilosc = models.IntegerField(default=1)    
+
+    def __str__(self):
+        return str(self.ta.nr)
+
+class Kolejnosc(models.Model):
+    tura = models.TextField()
+    data = models.DateField()
+
+    def __str__(self):
+        return_string = str(self.tura) + "(" + str(self.data) + ")"
+        return return_string
--- szwalnia/models.py
+++ szwalnia/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Wozek(models.Model):
+    wozek = models.IntegerField()
+    data_dodania = models.DateTimeField(default=timezone.now)
+    data_wydania = models.DateTimeField(null=True)
+    ta = models.ForeignKey(TA)
+    odebrany = models.BooleanField(default=False)    
+
+    def __str__(self):
+        return str(self.wozek)
